824696f19878dc6fff25da36b1239f878f5e6438
Levi Rogalla <levi.rogalla@gmail.com>
2025-03-02 22:42:23 -0500

    update docs

--- src/cli.sh
+++ src/cli.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
 # Path to your virtual environment
-VENV_PATH="/Users/levirogalla/Projects/ai-resume-builder/env"
+VENV_PATH="/Users/levirogalla/Projects/lib/ai-resume-builder/env"
 
 # Activate the virtual environment
 source "$VENV_PATH/bin/activate"
 
 # Run the Python script with arguments
-python3 /Users/levirogalla/Projects/ai-resume-builder/src/cli.py "$@"
+python3 /Users/levirogalla/Projects/lib/ai-resume-builder/src/cli.py "$@"
 
 # Deactivate the virtual environment
 deactivate
